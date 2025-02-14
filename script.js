6d59a4418f87226fa559ac91d467bc6decd75d5d
Anujpanwar109 <166197689+Anujpanwar109@users.noreply.github.com>
2025-02-14 09:44:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let a=document.querySelector("#colorSelect");
+let b=document.querySelector("input");
+b.addEventListener('click',()=>{
+	let selected=a.options[a.selectedIndex];
+	if(selected){
+		a.removeChild(selected);
+	}else{
+		alert("no option selected");
+	}
+});
